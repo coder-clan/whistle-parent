0b23a6076caf9b80c179c8aa08362f257195e61a
Aray Chou <Aray.Chou.CN@gmail.com>
2023-12-21 11:42:22 +0800

    fix: retry count not updated

--- whistle/src/main/java/org/coderclan/whistle/DatabaseEventPersistenter.java
+++ whistle/src/main/java/org/coderclan/whistle/DatabaseEventPersistenter.java
@@ -168,6 +168,7 @@ public class DatabaseEventPersistenter implements EventPersistenter {
                 tempList.add(new Event(rs.getString(1), type, eventContent));
             }
             rs.close();
+            conn.commit();
             return tempList;
         } catch (Exception e) {
             log.error("", e);
