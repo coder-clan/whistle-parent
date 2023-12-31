e1faa0c4a08a791d07a8546563b636612bb56e61
Aray Chou <Aray.Chou.CN@gmail.com>
2023-12-31 12:51:32 +0800

    change EventContent to interface from class

--- whistle-api/src/main/java/org/coderclan/whistle/api/EventContent.java
+++ whistle-api/src/main/java/org/coderclan/whistle/api/EventContent.java
@@ -1,43 +1,9 @@
 package org.coderclan.whistle.api;
 
 import java.io.Serializable;
-import java.time.Instant;
-import java.util.Objects;
-import java.util.UUID;
 
 /**
  * @author aray(dot)chou(dot)cn(at)gmail(dot)com
  */
-public abstract class EventContent implements Serializable {
-
-    private String idempotentId = UUID.randomUUID().toString();
-
-    private Instant time = Instant.now();
-
-    /**
-     * @return Idempotent ID, could be used to check if duplication of Events.
-     */
-    public String getIdempotentId() {
-        return idempotentId;
-    }
-
-    /**
-     * @return Creating time of the Event, could be used to check the order of Events.
-     */
-    public Instant getTime() {
-        return time;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EventContent content = (EventContent) o;
-        return Objects.equals(idempotentId, content.idempotentId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idempotentId);
-    }
+public interface EventContent extends Serializable {
 }
